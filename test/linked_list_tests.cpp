d6c9e80f6649b158c959ddd903cae33d069b2b0c
Ian R. O'Brien <ian.obrien@redpill-linpro.com>
2018-06-07 13:39:26 +0200

    add IsEmpty to linked_list

--- test/linked_list_tests.cpp
+++ test/linked_list_tests.cpp
@@ -5,11 +5,19 @@
 #include "../src/linked_list.h"
 #include "gtest/gtest.h"
 
-TEST(LinkedListTest, AddAndCountSize) {
+TEST(LinkedListTest, DefaultConstructor) {
     LinkedList<int> linkedList;
     ASSERT_EQ(linkedList.size(), 0);
-    linkedList.Add(new int32_t(2));
+    ASSERT_TRUE(linkedList.IsEmpty());
+}
+
+TEST(LinkedListTest, AddAndCountSize) {
+    LinkedList<int> linkedList;
+    linkedList.Add(new int32_t(0));
     ASSERT_EQ(linkedList.size(), 1);
+    
+    linkedList.Add(new int32_t(1));
+    ASSERT_EQ(linkedList.size(), 2);
 }
 
 
